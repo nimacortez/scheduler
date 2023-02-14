d6538f12557e6bd5d9c46f19291f187247dcc25b
Nima H <nimahz@Nimas-MacBook-Air.local>
2023-02-14 18:57:46 -0400

    added day list item and day list

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+function formatSpots(spots) {
+  if (spots === 0) {
+    return `no spots remaining`;
+  } else if (spots === 1) {
+    return `1 spot remaining`;
+  } else {
+  return `${spots} spots remaining`;
+  }
+};
+
+export default function DayListItem(props) {
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
