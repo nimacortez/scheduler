02d62f013ad5fde1df3b6048a6144819df28e6fc
Nima H <nimahz@Nimas-MacBook-Air.local>
2023-03-28 21:06:17 -0400

    fixed errors in testing

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,9 +15,9 @@ export default function useApplicationData(){
 
   useEffect(()=> {
     Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`),
+      axios.get(`http://localhost:8001/api/interviewers`),
     ]).then((all) => {
       setState(prev => (
         {...prev, days: all[0].data, 
